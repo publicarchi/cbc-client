83d3948ef5e778c5232540bb6f2681a28eee1316
emchateau <emchateau@laposte.net>
2021-12-31 11:10:10 -0500

    amélioration de la gestion des erreurs

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,7 +1,20 @@
-<h2>
-	<span class="errorCode">404</span>
-	<span class="errorMessage">Page Not Found</span>
-</h2>
+<script context="module" lang="ts">
+	/** @type {import('@sveltejs/kit').ErrorLoad} */
+	export function load({ error, status }) {
+		return {
+			props: {
+				title: `${status}: ${error.message}`
+			}
+		};
+	}
+</script>
+
+<script>
+	export let title;
+</script>
+
+<h1>{title}</h1>
+
 
 <style>
 	h2 {
