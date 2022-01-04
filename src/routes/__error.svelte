911d6efc3bb0cc3f2d7edb66f93376b7712ca63d
emchateau <emchateau@laposte.net>
2022-01-03 23:19:16 -0500

    amélioration des erreurs

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,36 +1,54 @@
-<script context="module" lang="ts">
-	/** @type {import('@sveltejs/kit').ErrorLoad} */
-	export function load({ error, status }) {
+<script context="module">
+	export function load({ status, error }) {
 		return {
-			props: {
-				title: `${status}: ${error.message}`
-			}
+			props: { status, error }
 		};
 	}
 </script>
 
 <script>
-	export let title;
-	import {Content} from 'carbon-components-svelte'
+	import { 
+		Content,
+		InlineNotification
+	} from 'carbon-components-svelte';
+	import { dev } from '$app/env';
+	export let status;
+	export let error;
+	// we don't want to use <svelte:window bind:online> here, because we only care about the online
+	// state when the page first loads
+	let online = typeof navigator !== 'undefined' ? navigator.onLine : true;
 </script>
 
-<Content>
-	<h1>{title}</h1>
-</Content>
+<svelte:head>
+	<title>{status}</title>
+</svelte:head>
 
-<style>
-	h2 {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		font-weight: 400;
-		min-height: 400px;
-	}
-	.errorCode {
-		font-size: 8rem;
-	}
-	.errorMessage {
-		font-size: 1rem;
-	}
-</style>
+<Content>
+	{#if online}
+		<h1>Yikes!</h1>
+
+		{#if error.message}
+			<InlineNotification lowContrast kind="error"
+ title="Error:" subtitle="{status}: {error.message}" />
+		{:else}
+		<InlineNotification lowContrast kind="error"
+ title="Error:" subtitle="Encountered a {status} error" />
+		{/if}
+
+		{#if dev && error.stack}
+			<pre>{error.stack}</pre>
+		{:else}
+			{#if status >= 500}
+				<p>Please try reloading the page.</p>
+			{/if}
+
+			<p>
+				If the error persists, please raise an issue on <a href="https://github.com/publicarchi/cbc-client">github</a>. Thanks!
+			</p>
+		{/if}
+	{:else}
+		<h1>It looks like you’re offline</h1>
+
+		<p>Reload the page once you’ve found the internet.</p>
+	{/if}
+</Content>
