3ad74c9acafdb175d9c122e8722272df4a6e5336
William Diakité <william.diakite@gmail.com>
2022-04-14 09:38:11 -0400

    implements affaires pages

--- src/routes/affaires/[conbavilId].svelte
+++ src/routes/affaires/[conbavilId].svelte
@@ -1,21 +1,17 @@
 <script context="module" lang="ts">
 	export async function load({ fetch, params }) {
-		const id = params.conbavilId
-		let affaire
-
-		console.log('Affaire ID', id)
-
-		fetch(`http://127.0.0.1:8984/cbc/affairs/${id}`)
-			.then((res) => res.json())
-			.then((data) => (affaire = data.content))
-			.catch((err) => console.log(err))
+		console.log(`http://127.0.0.1:8984/cbc/affaires/${params.conbavilId}`)
+		const response = await fetch(`http://127.0.0.1:8984/cbc/affaires/${params.conbavilId}`)
+		const affaire = await response.json()
 
 		return { props: { affaire } }
 	}
 </script>
 
 <script lang="ts">
+	import { Link } from 'carbon-components-svelte'
 	export let affaire
+	console.log(affaire)
 </script>
 
 <svelte:head>
@@ -23,3 +19,56 @@
 </svelte:head>
 
 <h1>{affaire.head}</h1>
+
+<h4>Localisation</h4>
+<div class="data-group">
+	<span class="data-group-label">Commune</span>
+	<span class="data-group-value">{affaire.localisation.commune}</span>
+</div>
+<div class="data-group">
+	<span class="data-group-label">Département</span>
+	<span class="data-group-value">{affaire.localisation.departement}</span>
+</div>
+<div class="data-group">
+	<span class="data-group-label">Département ancien</span>
+	<span class="data-group-value">{affaire.localisation.departementAncien}</span>
+</div>
+<div class="data-group">
+	<span class="data-group-label">Région</span>
+	<span class="data-group-value">{affaire.localisation.region}</span>
+</div>
+
+<h4>Types de l'affaire</h4>
+<span class="data-group-value">{affaire.types}</span>
+<ul>
+	{#each affaire.types as t}
+		<span class="data-group-value">{t}</span>
+	{/each}
+</ul>
+
+<h4>Délibérations</h4>
+<ul>
+	{#each affaire.deliberations as d}
+		<li>
+			<Link href="http://127.0.0.1:3000/deliberations/{d.id}">{d.title ? d.title : d.altTitle}</Link
+			>
+		</li>
+	{/each}
+</ul>
+
+<style>
+	h4 {
+		margin-top: 1em;
+		font-weight: 300;
+	}
+	.data-group {
+		display: flex;
+		margin-top: 0.5em;
+	}
+	.data-group-label {
+		font-weight: bold;
+	}
+	.data-group-value {
+		margin-left: 1em;
+	}
+</style>
--- src/routes/affaires/index.svelte
+++ src/routes/affaires/index.svelte
@@ -1,46 +1,162 @@
-<script lang="ts">
-	import { onMount } from 'svelte';
-	import { DataTable, Grid, Row, Column } from 'carbon-components-svelte';
-	import { v4 as uuidv4 } from 'uuid';
+<script context="module" lang="ts">
+	export async function load({ fetch }) {
+		let response = await fetch('http://127.0.0.1:8984/cbc/affaires')
+		let data = await response.json()
 
-	let affairs = [];
-	let meta = null;
+		return {
+			props: { affaires: data.content, meta: data.meta }
+		}
+	}
+</script>
+
+<script lang="ts">
+	import { onMount } from 'svelte'
+	import {
+		DataTable,
+		Grid,
+		Row,
+		Column,
+		Button,
+		Loading,
+		Link,
+		Pagination,
+		Toolbar,
+		ToolbarContent,
+		ToolbarSearch,
+		ToolbarMenu,
+		ToolbarMenuItem,
+		ToolbarBatchActions
+	} from 'carbon-components-svelte'
+	import { DocumentAdd16, Save16, Launch16 } from 'carbon-icons-svelte'
+	// import AffaireExpandedRow from '$components/AffaireExpandedRow.svelte'
+
+	export let affaires
+	export let meta
+
+	let adMap = {}
+
+	let searchQuery
+
+	let selectedRowIds = []
+	let expandedRowIds = []
+
+	onMount(async () => {
+		adMap = affaires.reduce(async (acc, a) => {
+			let deliberations = []
+			await Promise.all(
+				a.deliberations.map((id) => {
+					return fetch(`http://127.0.0.1:8984/cbc/deliberations/${id}`)
+						.then((res) => res.json())
+						.then((data) => deliberations.push(data))
+				})
+			)
+			acc[a.id] = deliberations
+		}, {})
+
+		console.log('adMap', adMap)
+	})
 
 	const fetchAffairs = () => {
-		fetch('http://127.0.0.1:8984/cbc/affairs')
+		fetch('http://127.0.0.1:8984/cbc/affaires')
 			.then((res) => res.json())
 			.then((data) => {
-				meta = data.meta;
+				meta = data.meta
 
 				// new ids for affairs and deliberation count
-				affairs = data.content.map((a) => ({
+				affaires = data.content.map((a) => ({
 					...a,
-					id: uuidv4(),
 					deliberationCount: a.deliberations.length
-				}));
+				}))
 			})
-			.catch((err) => console.log(err));
-	};
+			.catch((err) => console.log(err))
+	}
 
-	onMount(() => fetchAffairs());
+	const onPaginationUpdate = () => null
 
-	$: console.log(affairs);
-	$: console.log(meta);
+	const rowOnClick = (row) => {
+		if (expandedRowIds.includes(row.detail.id)) {
+			expandedRowIds.splice(expandedRowIds.indexOf(row.detail.id), 1)
+		} else {
+			expandedRowIds.push(row.detail.id)
+		}
+		//updates expandedRowIds
+		expandedRowIds = expandedRowIds
+	}
+
+	const getDeliberations = async (ids) => {
+		let deliberations = []
+		await Promise.all(
+			ids.map((id) => {
+				return fetch(`http://127.0.0.1:8984/cbc/deliberations/${id}`)
+					.then((res) => res.json())
+					.then((data) => deliberations.push(data))
+					.catch((err) => console.log(err))
+			})
+		)
+	}
 </script>
 
 <Grid>
 	<DataTable
+		on:click:row={rowOnClick}
 		title="Liste des affaires"
+		bind:selectedRowIds
+		bind:expandedRowIds
 		headers={[
-			{ key: 'id', value: 'ID' },
 			{ key: 'head', value: 'Titre' },
 			{ key: 'localisation.commune', value: 'Commune' },
 			{ key: 'localisation.departement', value: 'Département' },
 			{ key: 'localisation.departementAncien', value: 'Dépt. ancien' },
 			{ key: 'localisation.departementDecimal', value: 'Dépt. code' },
 			{ key: 'localisation.region', value: 'Région' },
-			{ key: 'deliberationCount', value: 'Délibérations' }
+			{ key: 'id', empty: true }
 		]}
-		rows={affairs}
-	/>
+		rows={affaires}
+	>
+		<svelte:fragment slot="cell" let:cell let:row>
+			{#if cell.key === 'id'}
+				<Link icon={Launch16} href="/affaires/{cell.value}" target="_blank" />
+			{:else}
+				{cell.value}
+			{/if}
+		</svelte:fragment>
+
+		<!-- <svelte:fragment slot="expanded-row" let:row>
+			<AffaireExpandedRow
+				deliberationIds={affaires.filter((a) => a.id === row.id)[0].deliberations}
+			/>
+		</svelte:fragment> -->
+		<Toolbar>
+			<ToolbarBatchActions>
+				<Button icon={DocumentAdd16}>Créer une affaire</Button>
+				<Button icon={Save16}>Exporter les fiches</Button>
+			</ToolbarBatchActions>
+			<ToolbarContent>
+				<ToolbarSearch expanded={true} persistent={true} bind:value={searchQuery} />
+				<ToolbarMenu>
+					<ToolbarMenuItem primaryFocus>Restart all</ToolbarMenuItem>
+					<ToolbarMenuItem href="https://cloud.ibm.com/docs/loadbalancer-service"
+						>API documentation</ToolbarMenuItem
+					>
+					<ToolbarMenuItem danger>Stop all</ToolbarMenuItem>
+				</ToolbarMenu>
+				<Button>Create balancer</Button>
+			</ToolbarContent>
+		</Toolbar>
+
+		<Pagination
+			on:update={onPaginationUpdate}
+			backwardText="Page précédente"
+			forwardText="Page suivante"
+			itemsPerPageText="Fiches par page :"
+			pageSizes={[20, 50, 100, 250, 500]}
+			bind:page={meta.currentPage}
+			bind:pageSize={meta.count}
+			bind:totalItems={meta.totalItems}
+		/>
+
+		{#if affaires.length === 0}
+			<Loading />
+		{/if}
+	</DataTable>
 </Grid>
