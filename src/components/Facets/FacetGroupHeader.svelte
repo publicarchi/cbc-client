34eef51cb75f650ee466de4123b0a68ac8513339
William Diakite <william.diakite@gmail.com>
2022-05-23 12:36:33 -0400

    adds FacetGroupHeader

---
+++ src/components/Facets/FacetGroupHeader.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import ChevronDown from 'carbon-icons-svelte/lib/ChevronDown.svelte'
+	import Reset from 'carbon-icons-svelte/lib/Reset.svelte'
+
+	export let collapseId: string
+
+	const collapse = (id: string): void => {
+		const div = document.getElementById(id)
+		div.hidden = !div.hidden
+	}
+</script>
+
+<div class="separator" />
+
+<div class="facet-group-header">
+	<h5>Localisation de l'affaire</h5>
+	<div class="facet-group-header-btns">
+		<div
+			class="btn"
+			on:click={() => reset(['commune', 'departement', 'departementAncien', 'region'])}
+		>
+			<Reset />
+		</div>
+		<div class="btn" on:click={() => collapse(collapseId)}>
+			<ChevronDown />
+		</div>
+	</div>
+</div>
